5c8cb8a6b91f4340c236c2a732940d21047ffdfc
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-03 12:35:37 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,4 +1,23 @@
 class Solution:
     def longestMountain(self, arr):
         
-        # code for Longest Mountain
+        n = len(arr)
+        if n < 3:
+            return 0
+        
+        up, down = [0] * n, [0] * n
+        maxLen = 0
+
+        for i in range(1, n):
+            if arr[i] > arr[i - 1]:
+                up[i] = up[i - 1] + 1
+
+        for i in range(n - 2, -1, -1):
+            if arr[i] > arr[i + 1]:
+                down[i] = down[i + 1] + 1
+
+        for i in range(n):
+            if up[i] > 0 and down[i] > 0:
+                maxLen = max(maxLen, up[i] + down[i] + 1)
+
+        return maxLen
